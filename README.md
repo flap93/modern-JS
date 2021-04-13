a7aa8d63855dfc1e66c91ef85b87d40fd929791f
flpalacios93@gmail.comgit config --global user.name flap93git config --global user.name flpalacios93@gmail.com <flpalacios93@gmail.com>
2021-04-12 21:58:44 -0400

    data types and types of conversion

--- README.md
+++ README.md
@@ -36,3 +36,58 @@ clear(); this cleans our console in google chrome
 time & timeEnd whatever we put between this two , it will tell us how long it took to run the lines of code ;
 
 warn , it prints a warning in our console 
+
+
+
+# VAR , LET , CONST
+
+with var or let we can reassing variables using var or let,
+we can  assign variable
+
+the only characters that a VAR can hold is letters , numbers , _ & $ .
+
+A variable can not start with a number so never name your variable with a number 
+
+the differences between them are that 
+VAR and LET are mutuable variables and 
+CONST is unmutuable variable 
+
+# what is Global scope and Local scope 
+
+Global means that can be accessed and modified anywhere in the program and local variables CANT be accessed outside of the functuon declaration . Global variable and local variable can have same name without affecting each other. JavaScript does not allow block level scope inside { } brackets.
+
+# what is scope 
+Scope in JavaScript refers to the current context of code, which determines the accessibility of variables to JavaScript. The two types of scope are local and global: Global variables are those declared outside of a block. Local variables are those declared inside of a block
+
+# Data types
+
+there are two types :
+
+- PRIMITIVE DATA TYPES :
+stored directly in the location the variable accesses stored on the stack
+
+- REFERENCE DATA types :
+Acceses by reference objects that are stored on the heap a pointer to a location in memory 
+
+* PRIMITIVE DATA TYPES :
+- STRING
+- NUMBER
+- BOOLEAN
+- NULL
+- UNDEFINED
+- SYMOBLS (ES6)
+
+* REFERENCES DATA TYPES:
+- ARRAYS
+- OBJECT LITERALS
+- FUNCTIONS
+- DATES
+- ANYTHING ELSE
+
+we can console.log(typeof _____) amd we this now the type of variable that is it.
+
+# TYPE CONVERSION
+
+.length only works on strings because is string method-propertie
+it gives you the nymber of characters on the string
+
--- app.js
+++ app.js
@@ -1,22 +1,138 @@
 // Log to console
+// DIFFERENT TYPES OF DATA THAT YOU CAN LOG INTO YOUR CONSOLE
 
 console.log('Hello world'); // text
 console.log('123');  // numbers
 console.log('true');  // booleans
 
-const greeting = 'Hello'; // variable
-console.log(greeting);   // log of the variable
-console.log([1, 2, 3, 4]);  // arrays
-console.log({a: 1, b:2});   // object literals
+//-----------------------------------------------------------
+// DIFERENT WAYS OF ASSIGN A VARIABLE VALUE
+// DIFERENT TYPES OF CONSOLE.LOG, CLEAR , WARN .... ETC
+//--------------------------------------------------------
 
-console.table({a: 1, b:2}); // it print the object in the console as table 
 
-console.error('this is some error'); // display our message in red as a error
+// const greeting = 'Hello'; // variable
+// console.log(greeting);   // log of the variable
+// console.log([1, 2, 3, 4]);  // arrays
+// console.log({a: 1, b:2});   // object literals
 
-console.warn('this a warning');
-console.clear();
-console.time('hello');
-console.log('Hello world');
-console.log('Hello world');
-console.log('Hello world');
-console.timeEnd('hello');
+// console.table({a: 1, b:2}); // it print the object in the console as table 
+
+// console.error('this is some error'); // display our message in red as a error
+
+// console.warn('this a warning');
+// console.clear();
+// console.time('hello');
+// console.log('Hello world');
+// console.log('Hello world');
+// console.log('Hello world');
+// console.timeEnd('hello');
+
+//----------------------------------------------------------------------------
+// ASSIGN VALUES OF VAR , LET AND CONST 
+// -----------------------------------------
+
+// var , let , const
+
+// var name = "John Doe";
+// console.log(name);
+// name = 'Marisol'
+// console.log(name);
+
+// init var
+
+// var greeting;
+// console.log(greeting);
+// greeting = 'Hello';
+// console.log(greeting);
+
+
+//  LET
+
+// let name;
+// name = "John Doe";
+// console.log(name);
+// name = 'Marisol'
+// console.log(name);
+
+// CONST (CONSTANT)
+
+// const name = 'John'
+// console.log(name);
+
+
+//-------------------------------------------------------------------
+// CREATING VARIABLES AND DIFERENT TYPES (type of )
+// ---------------------------------------
+
+// PRIMITIVE
+
+
+
+// STRING
+  const name = 'John Doe';
+
+  // NUMBER
+
+  const age = 45;
+
+  // BOOLEAN
+
+  const hasKids = true;
+
+  // NULL
+
+  const car = null;
+
+
+  // undefined 
+
+  let test ;
+
+  //  SYMBOL
+
+  //const sym = symbol();
+
+
+  console.log(typeof name); // whatever we replace for name with the value of data types on the top 
+  // it will give us the the type of PRIMITIVE data 
+
+
+
+// REFERENCES DATA TYPES 
+
+// OBJECTS
+
+const address = {
+    city: 'Boston',
+    state: 'Ma'
+}
+
+const today = new Date(); // this is an object too
+
+// ARRAY
+
+const hobbies = ['movies', 'music'];
+
+
+console.log(typeof address);
+
+
+// TYPES OF CONVERSION
+
+let val;
+
+// number to string 
+val = String(5); // to convert a number to a string we have to wrap it in a function string 
+// so for legnth to work , we put this into a string function and it will count the numbers inside
+// of the parenthesis in this case 1 because there is only one 5 
+
+// OR YOU CAN USE 
+
+val = (5).toString(); // whatever we pass inside will convert into a string
+
+// output
+
+console.log(val)
+console.log(typeof val)
+console.log(val.length);
